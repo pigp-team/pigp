a645a3c19e9a3e400879422cc20f12db5dc8f957
xinfeng.xu <xinfeng.xu@qunar.com>
2019-07-13 12:04:27 +0800

    remove user goal info

--- fans/src/main/java/com/feng/pigp/fans/service/EventLoopService.java
+++ fans/src/main/java/com/feng/pigp/fans/service/EventLoopService.java
@@ -12,7 +12,6 @@ import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
-import org.springframework.util.CollectionUtils;
 import javax.annotation.Resource;
 import java.util.List;
 import java.util.Set;
